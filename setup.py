2afd72217880479bd81a347202d8596ecb720e20
apolcyn <alexpolcyn@comcast.net>
2016-02-01 22:57:28 -0800

    renamed to pathclustering

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='traclus_impl',
+setup(name='pathclustering',
       version='0.1',
       description='First pass implementation of traclus algorithm',
       url='http://github.com/apolcyn/traclus_impl.git',
       author='AP',
       author_email='alexpolcyn65@gmail.com',
       license='MIT',
-      packages=['traclus_impl'],
+      packages=['pathclustering'],
       install_requires=[
           'planar',
       ],
