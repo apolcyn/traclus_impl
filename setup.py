d523c7e17c6783f75177b9184e2fbe6635127dac
apolcyn <alexpolcyn@comcast.net>
2016-02-01 22:21:02 -0800

    added in setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='traclus_impl',
+      version='0.1',
+      description='First pass implementation of traclus algorithm',
+      url='http://github.com/apolcyn/traclus_impl.git',
+      author='AP',
+      author_email='alexpolcyn65@gmail.com',
+      license='MIT',
+      packages=['traclus_impl'],
+      zip_safe=False)
