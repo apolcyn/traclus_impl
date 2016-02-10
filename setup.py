0f27fa69a6a33a8eb5b4d74281d69288ce89638a
apolcyn <alexpolcyn@comcast.net>
2016-02-09 22:57:53 -0800

    update to 0.7 version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='traclus_impl',
-      version='0.6',
+      version='0.7',
       description='First pass implementation of traclus algorithm',
       url='http://github.com/apolcyn/traclus_impl.git',
       author='AP',
