b54c124363f3cd7bab01cd6bc23a6b70d2972682
apolcyn <alexpolcyn@comcast.net>
2016-02-14 20:29:57 -0800

    added description to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup
 
+LONG_DESCRIPTION = """First pass implementation of traclus algorithm.
+This works with 2D trajectories. 
+See the github for the unit tests and integ tests.
+
+Note that this is being written as a piece of a senior project.
+The implementation should functionally work, but the distance
+functions need to be sped up and the code should be refactored
+for readability."""
+
+
 setup(name='traclus_impl',
-      version='0.7',
+      version='0.8',
       description='First pass implementation of traclus algorithm',
       url='http://github.com/apolcyn/traclus_impl.git',
       author='AP',
